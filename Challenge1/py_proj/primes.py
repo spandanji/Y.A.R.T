b9b83ede08924bdbb754c74205c604e0bc7cb77a
Jit77 <soumyo.mechanic@gmail.com>
2020-05-19 01:37:04 +0530

    Added python code

---
+++ Challenge1/py_proj/primes.py
@@ -0,0 +1,29 @@
+import time
+from itertools import compress
+
+def sieve(limit):
+
+    #if limit < 2:
+        #return 0
+    
+    st = time.time()
+    limit += 1
+    primes = [True] * limit
+
+    for i in range(2, int(limit ** 0.5 + 1)):
+        if primes[i]:
+            primes[i * i: limit: i] =  [False] * ((((limit - i*i) - 1) // i) + 1)
+
+    primes[0] = primes[1] = False
+
+    s = sum(compress(range(limit), primes))
+    ed = time.time()
+    print(f'Time: {ed - st: .4f}')
+
+    return s
+
+if __name__ == '__main__':
+
+    s = sieve(10 ** 8)
+    print(f'Sum: {s}')
+
