99c23310fee82ff376e306cabf5f6b7950728a73
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2020-05-21 02:16:52 +0530

    Added Challenge2 C++ Code

---
+++ Challenge2/cpp_proj/Readme.md
@@ -0,0 +1,3 @@
+### Challenge 2
+
+In this challenge we have to find the summation of all prime numbers from 1 to N using the Segmented Seive of Eratosthenes algorithm.
---
+++ Challenge2/cpp_proj/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cmath>
+#include <algorithm>
+
+uint64_t segmented_seive(const uint64_t& n)
+{
+    uint64_t sqrt_n = (uint64_t)std::sqrt(n);
+    const uint64_t seive_size = std::max(sqrt_n, (uint64_t)32768);
+    
+    std::vector<uint64_t> primes;
+    std::vector<uint64_t> multiples;
+    std::vector<uint_fast8_t> is_prime(sqrt_n + 1, 1);
+    std::vector<uint_fast8_t> seive(seive_size);
+
+    uint64_t sum = 2;
+    uint64_t is_prime_i = 3;
+    uint64_t seive_i = 3;
+    uint64_t num = 3;
+
+    for(uint64_t lower_limit = 0; lower_limit <= n; lower_limit += seive_size)
+    {
+        std::fill(seive.begin(), seive.end(), 1);
+        uint64_t upper_limit = lower_limit + seive_size - 1;
+        upper_limit = std::min(upper_limit, n);
+
+        
+        while(is_prime_i * is_prime_i <= upper_limit)
+        {
+            if(is_prime[is_prime_i])
+                for(uint64_t j = is_prime_i * is_prime_i; j <= sqrt_n; j += is_prime_i)
+                    is_prime[j] = 0;
+            is_prime_i += 2;
+        }
+        
+        while(seive_i * seive_i <= upper_limit)
+        {
+            if(is_prime[seive_i])
+            {
+                primes.push_back(seive_i);
+                multiples.push_back(seive_i * seive_i - lower_limit);
+            }
+            seive_i += 2;
+        }
+
+        for(uint64_t i = 0; i < primes.size(); i++)
+        {
+            uint64_t j = multiples[i];
+            for(uint64_t k = primes[i] * 2; j < seive_size; j += k)
+                seive[j] = 0;
+            multiples[i] = j - seive_size;
+        }
+
+        for(; num <= upper_limit; num += 2)
+            if(seive[num - lower_limit])
+                sum += num;
+    }
+    
+    return sum;
+}
+
+int main()
+{
+    uint64_t n; std::cin >> n;
+    auto start = std::chrono::steady_clock::now();
+    auto sum = segmented_seive(n);
+    auto end =  std::chrono::steady_clock::now();
+    std::cout << "TIME: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms\n";
+    std::cout << "SUM: " << sum << '\n';
+}
