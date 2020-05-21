e01302e9cafb505e394d6f32a66a9875447511f3
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2020-05-21 14:04:20 +0530

    Formatting and minor changes for Challenge 1 C++ code

--- Challenge1/cpp_proj/main.cpp
+++ Challenge1/cpp_proj/main.cpp
@@ -1,27 +1,32 @@
+#include <chrono>
 #include <iostream>
 #include <vector>
-#include <chrono>
 
-int main()
-{
-    unsigned int n; std::cin >> n;
+auto main() -> int {
+    unsigned int n = 0;
+    std::cin >> n;
     auto start = std::chrono::steady_clock::now();
     std::vector<uint8_t> primes(n + 1, 1);
     primes[1] = 0;
-    for(unsigned int i = 2; i * i <= n; i++)
-        if(primes[i])
-        {
+    for (unsigned int i = 2; i * i <= n; i++) {
+        if (primes[i] != 0u) {
             unsigned int j = i * i;
-            while(j <= n)
-            {
+            while (j <= n) {
                 primes[j] = 0;
                 j += i;
             }
         }
+    }
     unsigned long sum = 0;
-    for(unsigned int i = 1; i <= n; i++)
-        if(primes[i])
+    for (unsigned int i = 1; i <= n; i++) {
+        if (primes[i] != 0u) {
             sum += i;
-    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count() << "ms\n";
-    std::cout << "Sum: "<< sum << '\n';
+        }
+    }
+    std::cout << "Time: "
+        << std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::steady_clock::now() - start)
+        .count()
+        << "ms\n";
+    std::cout << "Sum: " << sum << '\n';
 }
